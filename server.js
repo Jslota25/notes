16296c949bdf22149f65cb73868631e038fe572f
John Slota <jslota17@comcast.net>
2020-04-23 23:00:47 -0500

    Express page functions

--- server.js
+++ server.js
@@ -12,3 +12,22 @@ const writeFileAsync = util.promisify(fs.writeFile);
 // Sets up Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Server is listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+
+//Sends user to notes page
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
+
+// Sends user to index page
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
